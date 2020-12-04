18eafb187cc4aa9b99715b4183757a0abfdea4ad
Galya <galina.bosinzon@raiffeisen.ru>
2020-12-04 10:56:15 +0300

    add electrovert

--- MvcApp/routes.jl
+++ MvcApp/routes.jl
@@ -1,5 +1,5 @@
 using Genie.Router
-using MainController, LemiController
+using MainController, LemiController, ElectrovertController
 
 route("/", MainController.main)
 route("/", MainController.frame, method="POST")
@@ -7,3 +7,7 @@ route("/", MainController.frame, method="POST")
 route("/lemi", LemiController.main)
 route("/lemi", LemiController.frame, method="POST")
 route("/lemi/download_trunc", LemiController.trunc, method="POST")
+
+route("/electrovert", ElectrovertController.main)
+route("/electrovert", ElectrovertController.frame, method="POST")
+route("/electrovert/download_trunc", ElectrovertController.trunc, method="POST")
